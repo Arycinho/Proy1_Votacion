bdba1b01cd42451431113bc097ee2e12223f7a12
Arycinho <aosdr13@gmail.com>
2022-09-22 17:09:49 -0500

    Probando add+commit+push

--- app/src/main/java/com/example/proy1_votacion/MainActivity.java
+++ app/src/main/java/com/example/proy1_votacion/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.proy1_votacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvtile, tv1, tv2, tvbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
